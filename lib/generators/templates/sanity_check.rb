37be33e923c0f1a0dff44c0d10d12151602974a4
sxjoel <joel@senexx.com>
2015-10-02 13:36:46 +0300

    bug fixes

--- lib/generators/templates/sanity_check.rb
+++ lib/generators/templates/sanity_check.rb
@@ -1,5 +1,15 @@
 SanityCheck.setup do |processes|
 
-	processes = []
+	#array of your environment's processes to check against
+	#for example:
+	#processes.names = ['redis', 'postgres', 'sidekiq']
+	processes.names = [] 
+
+	#Stops the server if any of the listed processes is not running
+	#processes.is_aggressive = true
+
+	#init
+	processes.check_processes
+
 
 end
--- lib/sanitycheck.rb
+++ lib/sanitycheck.rb
@@ -1,16 +1,23 @@
-module Sanitycheck
+module SanityCheck
 
-  @processes = []
+  class Processes
+    attr_accessor :names, :is_aggressive
+
+    def check_processes
+      names.each do |process_name|
+        id = system("pgrep -f #{process_name} > /dev/null 2>&1")
+        unless id
+          puts "SanityCheck: #{process_name} IS A DEPENDENCY YET IT IS NOT RUNNING!"
+        end
+      end
+      if is_aggressive
+        raise "SanityCheck:: YOU HAVE FORGOT TO PRE-RUN SOME PROCESSES"
+      end
+    end
+  end
 
   def self.setup
-    yield(@processes)
-    check_processes
+    yield(Processes.new)
   end
-
-  def check_processes 
-  	puts "HELLO BIATCH" 
-  	puts @processes 
-  end
-
 
 end
