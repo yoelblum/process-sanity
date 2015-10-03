006dbb58b6ec6d5da2b75e65f4b723c82750b9d8
sxjoel <joel@senexx.com>
2015-10-03 09:19:32 +0300

    must require process_sanity in initializer

--- test/dummy/config/initializers/process_sanity.rb
+++ test/dummy/config/initializers/process_sanity.rb
@@ -1,9 +1,11 @@
+require 'process_sanity'
+
 ProcessSanity.setup do |processes|
 
   #array of your environment's processes to check against
   #for example:
   #processes.names = ['redis', 'postgres', 'sidekiq']
-  processes.names = ['redis', 'sidekiq']
+  processes.names = []
 
   #Stops the server if any of the listed processes is not running
   #processes.is_aggressive = true
